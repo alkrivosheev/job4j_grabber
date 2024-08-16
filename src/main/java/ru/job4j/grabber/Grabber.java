7c7c2f653fb9198b1457738774fc597a250428a9
alkrivosheev <alkrivosheev@gmail.com>
2024-08-16 11:10:34 +0300

    WebStore

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -5,6 +5,9 @@ import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -16,12 +19,14 @@ public class Grabber implements Grab {
     private final Store store;
     private final Scheduler scheduler;
     private final int time;
+    private final int port;
 
-    public Grabber(Parse parse, Store store, Scheduler scheduler, int time) {
+    public Grabber(Parse parse, Store store, Scheduler scheduler, int time, int port) {
         this.parse = parse;
         this.store = store;
         this.scheduler = scheduler;
         this.time = time;
+        this.port = port;
     }
 
     @Override
@@ -40,6 +45,27 @@ public class Grabber implements Grab {
                 .withSchedule(times)
                 .build();
         scheduler.scheduleJob(job, trigger);
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(port)) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     public static class GrabJob implements Job {
@@ -66,6 +92,9 @@ public class Grabber implements Grab {
         var parse = new HabrCareerParse(new HabrCareerDateTimeParser());
         var store = new PsqlStore(config);
         var time = Integer.parseInt(config.getProperty("time"));
-        new Grabber(parse, store, scheduler, time).init();
+        var port = Integer.parseInt(config.getProperty("port"));
+        Grabber grab = new Grabber(parse, store, scheduler, time, port);
+        grab.init();
+        grab.web(store);
     }
 }
